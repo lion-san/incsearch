f2434bfe5f3e4201f87385d016ad5a9e65372690
yokoi.shinya <shinya.yokoi@gmail.com>
2015-03-06 10:59:11 +0900

    init.rb

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
-#require_dependency 'customers_hook_listener'
+require_dependency 'customers_hook_listener'
 Redmine::Plugin.register :incsearch do
   name 'Incsearch plugin'
   author 'Shinya Yokoi'
   description 'Redmine with incremental search'
-  version '0.9.5'
+  version '0.9.6'
 end
